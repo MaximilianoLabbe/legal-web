05add8ccefbf6dba8473aada78d7a71a4ef468f1
FGN Abogados Dev <development@fgn-abogados.com>
2026-05-05 01:05:39 -0400

    fix: change logout redirect from /login to /

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -27,7 +27,7 @@ export const Sidebar: React.FC = () => {
 
   const handleLogout = () => {
     logout();
-    window.location.href = '/login';
+    window.location.href = '/';
   };
 
   const filteredItems = navItems.filter(
